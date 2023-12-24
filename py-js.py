0ed7e67433cfc11e9f64dc110198fe7279f279a3
undefined <johann@objekt.stream>
2023-12-24 08:18:17 +0000

    ~

--- py-js.py
+++ py-js.py
@@ -5,8 +5,8 @@ from typing import Callable
 from javascript import require
 
 
-js_test: Callable[..., int] = require('./js/index.js')
+js_test: Callable[..., int] = require('./js/index.cjs')
 print(f"got {js_test()} from js in py")
 
-ts_test: Callable[..., int] = require('./js/index.ts').default
+ts_test: Callable[..., int] = require('./js/index.cts').default
 print(f"got {ts_test()} from ts in py")
